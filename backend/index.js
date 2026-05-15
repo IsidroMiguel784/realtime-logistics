23c467dae7e844530cfd444d6ec19194d43a8028
isidro <isidrohonrubia784@gmail.com>
2026-05-15 12:50:55 +0200

    Add packages API endpoints

--- backend/index.js
+++ backend/index.js
@@ -2,8 +2,33 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
+const packages = [
+    {
+        id: 1,
+        status: "IN_TRANSIT"
+    },
+    {
+        id: 2,
+        status: "DELIVERED"
+    }
+];
+
 app.get("/", (req, res) => {
-    res.send("Servidor funcionando 🚀");
+    res.send("API funcionando 🚀");
+});
+
+app.get("/packages", (req, res) => {
+
+    const newPackage = req.body;
+
+    packages.push(newPackage);
+
+    res.status(201).json({
+        message: "Paquete creado correctamente",
+        data: newPackage
+    });
 });
 
 app.listen(3000, () => {
