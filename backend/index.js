ed5b04a6b9b86ba02ed0ab0f6ab66119b84b840b
isidro <isidrohonrubia784@gmail.com>
2026-05-23 22:01:40 +0200

    Implement full CRUD for packages API

--- backend/index.js
+++ backend/index.js
@@ -31,6 +31,67 @@ app.get("/packages", (req, res) => {
     });
 });
 
+app.get("/packages/:id", (req, res) => {
+
+    const packageId = parseInt(req.params.id);
+
+    const foundPackage = packages.find(pkg => pkg.id === packageId);
+
+    if (!foundPackage) {
+        return res.status(404).json({           //404 -> codigo correcto cuando algo no existe
+            message: "Paquete no encontrado"
+        });
+    }
+
+    res.json(foundPackage);
+
+});
+
+app.put("/packages/:id", (req, res) => {
+
+    const packageId = parseInt(req.params.id);
+
+    const packageIndex = packages.findIndex(pkg => pkg.id === packageId);
+
+    if (packageIndex === -1) { //no encontrado
+        return res.status(404).json({
+            message: "Paquete no encontrado"
+        });
+    }
+
+    packages[packageIndex] = {
+        ...packages[packageIndex], //copia todas las propiedades del paquete existente en una posicion especifica
+        ...req.body //extrae todas las propiedades que vienen en la peticion HTTP
+                    //... -> ENVUELVE TODO PARA CREAR UN NUEVO OBJETO
+    };
+
+    res.json({
+        message: "Paquete actualizado correctamente",
+        data: packages[packageIndex]
+    });
+})
+
+app.delete("/packages/:id", (req, res) => {
+
+    const packageId = parseInt(req.params.id);
+
+    const packageIndex = packages.findIndex(pkg => pkg.id === packageId);
+
+    if (packageIndex === -1) {
+        return res.status(404).json({
+            message: "Paquete no encontrado"
+        });
+    }
+
+    packages.splice(packageIndex, 1); // elimina 1 elemento en esa posición
+
+    res.json({
+        message: "Paquete eliminado correctamente"
+    });
+});
+
+
 app.listen(3000, () => {
     console.log("Servidor ejecutándose en puerto 3000");
 });
+
